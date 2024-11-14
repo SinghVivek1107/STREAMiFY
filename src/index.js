d221ea50c7266d4f071ff7b3dde0f2cfb770cb85
SinghVivek1107 <viveksinghvs1107@gmail.com>
2024-11-14 15:46:13 +0530

    MongoDb Configuration Setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-import {app} from './app.js'
+import dotenv from "dotenv";
+dotenv.config({});
+
+import connectDB from "./db/dbconnect.js";
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    console.log(`App is listening on port ${PORT}`);
+  })
+  .catch((error) => {
+    console.log("MongoDB Connection Failed: ", error);
+  });
+
+
+  
+
+/*(async () => {
+    try {
+      await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+      console.log("Connected to MongoDB");
+
+      app.on("error", (error) => {
+        console.log("SERVER ERROR: ", error);
+      });
+
+      app.listen(PORT, () => {
+        console.log(`App is listening on port ${PORT}`);
+      });
+    } catch (error) {
+      console.log("CONNECION ERROR: ", error);
+      throw error;
+    }
+  })();*/
