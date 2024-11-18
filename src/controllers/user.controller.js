8698e87cf147a5c44688e09cba9274124c54e84b
SinghVivek1107 <viveksinghvs1107@gmail.com>
2024-11-19 00:39:18 +0530

    Made some changes in the avatar and local path

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,11 +22,23 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "Username or Email already exist");
   }
 
-  const avatarLocalPath = req.files?.avatar?.[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+  //console.log("Files:" ,req.files);
+
+  const avatarLocalPath = req.files?.avatar[0]?.path; //this path is where multer is uploading file at diskstorage
+
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
-    throw new ApiError(400, "Avatar is Required");
+    throw new ApiError(400, "Avatar is required");
   }
 
   const avatar = await uploadOnCloudinary(avatarLocalPath);
