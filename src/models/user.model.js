2c4bc34237ab7dd3502e0859f1418e2b5a470bfe
SinghVivek1107 <viveksinghvs1107@gmail.com>
2024-11-19 00:40:07 +0530

    Some minor changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -85,4 +87,4 @@ userSchema.methods.generateRefreshToken = function () {
   );
 };
 
-export const User = mongoose.Model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
