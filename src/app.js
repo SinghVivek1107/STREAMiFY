cd3156bd1eebe08121802369d649c25fe63daf45
SinghVivek1107 <viveksinghvs1107@gmail.com>
2024-11-18 21:43:24 +0530

    check if the server is running correcly or not

--- src/app.js
+++ src/app.js
@@ -1,19 +1,31 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express from "express"
+import express from "express";
 
 const app = express();
 
-app.use(cors({
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit:"16kb"}))
-app.use(express.static("public"))
-
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 app.use(cookieParser());
 
+//import routes
+import userRouter from "./routes/user.routes.js";
 
-export{app};
+//routes
+app.use("/api/v1/user", userRouter);
+
+// http://localhost:8000/api/v1/users/register
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -2,34 +2,16 @@ import dotenv from "dotenv";
 dotenv.config({});
 
 import connectDB from "./db/dbconnect.js";
+import { app } from "./app.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 connectDB()
   .then(() => {
-    console.log(`App is listening on port ${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`App is listening on port ${PORT}`);
+    });
   })
   .catch((error) => {
     console.log("MongoDB Connection Failed: ", error);
   });
-
-
-  
-
-/*(async () => {
-    try {
-      await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-      console.log("Connected to MongoDB");
-
-      app.on("error", (error) => {
-        console.log("SERVER ERROR: ", error);
-      });
-
-      app.listen(PORT, () => {
-        console.log(`App is listening on port ${PORT}`);
-      });
-    } catch (error) {
-      console.log("CONNECION ERROR: ", error);
-      throw error;
-    }
-  })();*/
