03a2769eda56137006790f6ddf93f863355e0d18
SinghVivek1107 <viveksinghvs1107@gmail.com>
2024-12-06 23:42:03 +0530

    Schema for comment model

---
+++ src/models/comment.model.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: mongoose.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+/**
+ * Mongoose aggregate paginate simplifies fetching and processing
+ * large datasets by combining advanced queries with pagination for
+ * better performance and user experience.
+ * Pagination Example:
+ * const options = { page: 1, limit: 10 };
+ * const aggregate = Comment.aggregate([{ $match: { video: videoId } }]);
+ * const paginatedResults = await Comment.aggregatePaginate(aggregate, options);
+ */
+
+commentSchema.plugin(mongooseAggregatePaginate);
+
+export const Comment = mongoose.model("Comment", commentSchema);
